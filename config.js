2909b3204c0c24838c2e5f0846e1a83d2f91dd9c
abayomipeteroyenuga-commits <abayomipeteroyenuga@gmail.com>
2026-09-16 08:47:34 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -3,5 +3,6 @@ window.ETHAN_HUB_CONFIG={
   supabasePublishableKey:'sb_publishable_LghFHw2wsTICWz3FmjFBew_zjk62iA_',
   erpUrl:'https://app.ethandigitalacademy.org',
   officeUrl:'https://office.ethandigitalacademy.org',
+  aiUrl:'https://search.ethandigitalacademy.org',
   ssoFunctionUrl:'https://hsigpjyvuvqdmujklcvw.supabase.co/functions/v1/ethan-sso'
 };
